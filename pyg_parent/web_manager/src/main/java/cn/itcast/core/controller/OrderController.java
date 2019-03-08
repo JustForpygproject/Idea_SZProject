294b9567042d184a5efc44b4029a41a4d2e05ef5
xiaoming <123@qq.com>
2019-03-08 15:10:53 +0800

    D-1001

---
+++ pyg_parent/web_manager/src/main/java/cn/itcast/core/controller/OrderController.java
@@ -0,0 +1,23 @@
+package cn.itcast.core.controller;
+
+import cn.itcast.core.pojo.entity.PageResult;
+import cn.itcast.core.pojo.order.Order;
+import cn.itcast.core.service.OrderService;
+import com.alibaba.dubbo.config.annotation.Reference;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/order")
+public class OrderController {
+    @Reference
+    private OrderService orderService;
+
+    @RequestMapping("/search")
+    public PageResult search(Integer page, Integer rows, @RequestBody Order order) {
+        PageResult pageResult =orderService.findPage(order, page, rows);
+        return pageResult;
+    }
+
+}
